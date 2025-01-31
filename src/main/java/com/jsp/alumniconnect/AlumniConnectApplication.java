ed851e39ea326cae5a5a28f53b103d01851f8fa0
Shrikant Naik <nshrikant31@gmail.com>
2025-01-31 18:26:38 +0530

    WAIT: Hiding JPA Dependancy From pom.xml

--- src/main/java/com/jsp/alumniconnect/AlumniConnectApplication.java
+++ src/main/java/com/jsp/alumniconnect/AlumniConnectApplication.java
@@ -8,6 +8,7 @@ public class AlumniConnectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlumniConnectApplication.class, args);
+		System.out.println("Way To Connect To Your Alumni");
 	}
 
 }
